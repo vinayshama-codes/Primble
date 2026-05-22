72330da06fe13661cf0e2bd462e08300db8ea531
Vinay Sharma <vinaysharma@astreait.com>
2026-05-22 15:46:17 +0530

    action fixed 2

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,7 +23,9 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-useless-escape': 'warn',
+      'no-empty': 'warn',
     },
   },
 ])
